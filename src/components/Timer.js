3610aff5440a346a60102694037459b4daca29d2
WhirlyByrd <kara.r.gardner@gmail.com>
2023-01-18 17:45:43 -0600

    added circle progress bar svg, and play button

---
+++ src/components/Timer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
+
+const red = '#f54e4e';
+const green = '#4aec8c'
+
+function Timer() {
+  return (
+    <div>
+     <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
+    textColor:'fff',
+    pathcolor:red,
+    tailColor:'rgba(255,255,255,.2)',
+    })} />
+
+    <div>
+      <PlayButton />
+    </div>
+    
+    </div>
+  );
+}
+
+export default Timer;
