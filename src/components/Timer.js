c67090ee7d2f94225d208a88e04b5459c925dabe
WhirlyByrd <kara.r.gardner@gmail.com>
2023-01-18 17:57:46 -0600

    settings button added

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
 
 const red = '#f54e4e';
 const green = '#4aec8c'
@@ -17,8 +19,11 @@ function Timer() {
 
     <div>
       <PlayButton />
+      <PauseButton />
+      <SettingsButton />
     </div>
     
+
     </div>
   );
 }
